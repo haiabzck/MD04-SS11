cca2ac59188e11d7758336d10b4db460589edf92
haiabzck <dinhhaiqblt1994@gmail.com>
2026-08-17 15:05:26 +0900

    new

--- session11/src/main/java/com/example/session11/dto/response/RegisterResponse.java
+++
@@ -1,4 +0,0 @@
-package com.example.session11.dto.response;
-
-public class RegisterResponse {
-}
--- session11/src/main/java/com/example/session11/dto/response/UserRegisterResponse.java
+++ session11/src/main/java/com/example/session11/dto/response/UserRegisterResponse.java
@@ -8,12 +8,8 @@ import lombok.*;
 @Setter
 @Builder
 public class UserRegisterResponse {
-
     private String username;
-
     private String fullName;
-
     private String email;
-
     private String role ;
 }
